877a3fb05dce7270524f83e771a9e4187d85c6d6
Megan Coyle <meg@mcoyle.com>
2016-07-08 14:08:30 -0400

    Add pseudo code, jquery, and initial bg image

--- script/script.js
+++ script/script.js
@@ -1,1 +1,19 @@
-console.log("got it");
+$( document ).ready(function() {
+    // set up variables
+    // keep score
+    // number card that user is on
+    // array of possible cards to choos from
+
+    // user clicks intro title to get started
+    // first flash card is shown
+    // with user input field and submit button
+
+    // on click of submit, user input is evaluated
+    // if matches answer string
+        // add point to user's score
+        // add congrats message
+
+    // if wrong, say sorry, incorrect
+
+    // after card is answered, display other side of flash card with answered
+});
