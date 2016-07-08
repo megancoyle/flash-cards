46de40f5316c2f3a03c84bbfb839049d14885dc2
Megan Coyle <meg@mcoyle.com>
2016-07-08 17:42:43 -0400

    Add card template

--- js/script.js
+++ js/script.js
@@ -1,20 +1,37 @@
 $( document ).ready(function() {
-    // set up variables
-    // keep score
-    // number card that user is on
-    // array of possible cards to choose from
-    // function to shuffle through arrays every time game is played
+  // user moves from title page to game page
+  var $title = $("#title");
+  var $card = $("#card");
+  var $score = $("#score");
 
-    // user clicks intro title to get started
-    // first flash card is shown
-    // with user input field and submit button
+  $title.on("click", showGameBoard);
 
-    // on click of submit, user input is evaluated
-    // if matches answer string
-        // add point to user's score
-        // add congrats message
+  function showGameBoard() {
+    $title.remove();
+    $card.show();
+    $score.show();
+  }
 
-    // if wrong, say sorry, incorrect
+  //Handlebars Template variables
+  var source = $("#card-template").html();
+  var template = Handlebars.compile(source);
 
-    // after card is answered, display other side of flash card with answered
+  // set up variables
+  // keep score
+  // number card that user is on
+  // array of possible cards to choose from
+  // function to shuffle through arrays every time game is played
+
+  // user clicks intro title to get started
+  // first flash card is shown
+  // with user input field and submit button
+
+  // on click of submit, user input is evaluated
+  // if matches answer string
+      // add point to user's score
+      // add congrats message
+
+  // if wrong, say sorry, incorrect
+
+  // after card is answered, display other side of flash card with answered
 });
